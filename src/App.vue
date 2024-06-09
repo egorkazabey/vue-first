de0586096c569a7060ec1f8d20eaffe46cdb70cd
Yehor Havrylov <egoregorgavrilov@gmail.com>
2024-06-09 12:44:29 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<post-form :posts='posts'/>
+		<post-list :posts="posts"/>
+	</div>
+</template>
+
+<script>
+	import PostList from '@/components/PostList.vue'
+	import PostForm from '@/components/PostForm.vue'
+	export default {
+		components: {
+			PostList, PostForm
+		},
+		data() {
+			return {
+				posts: [
+					{id: 1, title: 'JavaScript', body: 'Описание поста'},
+					{id: 2, title: 'JavaScript', body: 'Описание поста 2'},
+					{id: 3, title: 'JavaScript', body: 'Описание поста 3'},
+					{id: 4, title: 'JavaScript', body: 'Описание поста 4'},
+				],
+				title: '',
+				body: '',
+			}
+		}
+	}
+</script>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,76 @@
+<template>
+	<div>
+		<form @submit.prevent>
+			<h4>Создание поста</h4>
+			<input
+				v-bind:value='post.title'
+				@input='post.title = $event.target.value'
+				class='input'
+				type='text'
+				placeholder='Название'
+			/>
+			<input
+				v-bind:value='post.body'
+				@input='post.body = $event.target.value'
+				value='body'
+				class='input'
+				type='text'
+				placeholder='Описание'
+			/>
+			<button class='btn' @click='createPost'>Создать пост</button>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		posts: {
+			type: Array,
+		}
+	},
+	data() {
+		return {
+			post: {
+				title: '',
+				body: '',
+			}
+		}
+	},
+	methods: {
+		createPost() {
+			const newPost = {
+				id: Date.now(),
+				title: this.title,
+				body: this.body,
+			}
+			this.posts.push(newPost)
+			this.title = '';
+			this.body = '';
+		},
+	}
+}
+</script>
+
+<style scoped>
+	form {
+		display: flex;
+		flex-direction: column;
+		
+	}
+	.btn {
+		align-self: flex-end;
+		margin-top: 15px;
+		padding: 10px 15px;
+		background: none;
+		border: 1px solid teal;
+		cursor: pointer;
+	}
+	
+	.input {
+		width: 100%;
+		border: 1px solid #ccc;
+		padding: 10px 15px;
+		margin-top: 15px;
+	}
+</style>
